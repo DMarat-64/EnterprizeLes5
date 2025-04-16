a6c39bd2266fb71c428ed83caac27e03e34a1571
Marat D <Marat@mail.ru>
2025-04-16 23:20:24 +0400

    added .test

--- src/test/java/ShouidBeSoftAITest.java
+++ src/test/java/ShouidBeSoftAITest.java
@@ -1,7 +1,7 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,8 +11,10 @@ public class ShouidBeSoftAITest {
     void ShouidBeSoftHeadingTest(){
 
         open("https://github.com/");
+        $(".HeaderMenu-nav").shouldHave(text("Solutions"));
        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
        $$("[aria-labelledby=solutions-by-company-size-heading]").first().$("a").click();
-        $("#hero-section-brand-heading").shouldHave(Condition.text("The AI-powered developer platform"));
+       $(".lp-SubNavContainer").shouldHave(text("Enterprise"));
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
     }
 }
